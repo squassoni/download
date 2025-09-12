07384d67e1826728a592365a81a6a1b2f73a93f6
squassoni <brunosquassoni@hotmail.com>
2025-09-12 15:01:30 +0100

    Create repo.sh

---
+++ repo.sh
@@ -0,0 +1,11 @@
+# Repositório principal (OSS - software livre)
+sudo zypper ar -f http://download.opensuse.org/distribution/leap/15.4/repo/oss/ repo-oss
+
+# Repositório de software proprietário (Non-OSS)
+sudo zypper ar -f http://download.opensuse.org/distribution/leap/15.4/repo/non-oss/ repo-non-oss
+
+# Repositório de atualizações (OSS)
+sudo zypper ar -f http://download.opensuse.org/update/leap/15.4/oss/ repo-update-oss
+
+# Repositório de atualizações (Non-OSS)
+sudo zypper ar -f http://download.opensuse.org/update/leap/15.4/non-oss/ repo-update-non-oss
